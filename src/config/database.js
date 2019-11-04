f0a7e7eb8e68815dce6669fb01ebcdae8aa04443
natandias <49736127+natandias@users.noreply.github.com>
2019-11-04 10:25:05 -0300

    Add dotenv

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv')
+dotenv.config();
+
+module.exports = {
+  database: process.env.DATABASE,
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  host: process.env.HOST,
+  dialect: process.env.DIALECT,
+};
+
